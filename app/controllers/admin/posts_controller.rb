6300678af629c9613c51341f4b257d7eac8cfdc2
suzu784 <s.suzuki784@hotmail.com>
2022-07-08 06:47:26 +0000

    #35

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,18 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
   
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+  
 end
