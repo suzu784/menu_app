7ee70866c4ad5daa52488213cd8ff70e5fa97d71
suzu784 <s.suzuki784@hotmail.com>
2022-07-08 14:53:23 +0000

    #40

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_search
-  
-  def set_search
-    @search = Post.ransack(params[:q])
-    @posts = @search.result
-  end
 end
