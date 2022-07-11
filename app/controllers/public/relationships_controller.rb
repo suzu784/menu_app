cb89419096b71cd9b79c8d2c8fbbb16ea14e729d
suzu784 <s.suzuki784@hotmail.com>
2022-07-11 13:40:36 +0000

    #62

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,13 +2,13 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-    customer = Customer.find(params[:customer_id])
-    current_customer.follow(customer)
-    customer.create_notification_follow!(current_customer)
+    @customer = Customer.find(params[:customer_id])
+    current_customer.follow(@customer)
+    @customer.create_notification_follow!(current_customer)
   end
 
   def destroy
-    customer = Customer.find(params[:customer_id])
-    current_customer.unfollow(customer)
+    @customer = Customer.find(params[:customer_id])
+    current_customer.unfollow(@customer)
   end
 end
