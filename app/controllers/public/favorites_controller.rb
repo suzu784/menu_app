9d88740833eef8f75a44355204fec8d612a6e5a5
suzu784 <85116099+suzu784@users.noreply.github.com>
2022-07-14 11:18:29 +0900

    Update favorites_controller.rb

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,7 +5,6 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     @favorite = current_customer.favorites.new(post_id: @post.id)
     @favorite.save
-    
     @post.create_notification_by!(current_customer)
     respond_to do |format|
       format.html {redirect_to request.referrer}
