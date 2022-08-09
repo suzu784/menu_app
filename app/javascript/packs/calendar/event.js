c0c780ae5f5a828a498ba9fa274a093c6c53d6a9
suzu784 <s.suzuki784@hotmail.com>
2022-08-09 02:09:28 +0000

    #133

--- app/javascript/packs/calendar/event.js
+++ app/javascript/packs/calendar/event.js
@@ -32,6 +32,7 @@ document.addEventListener('turbolinks:load', function() {
         allDayText: '終日',
         height: "auto",
         editable: true,
+        displayEventTime: false,
 
         eventDrop: function(info) {
             $.ajax({
@@ -45,49 +46,24 @@ document.addEventListener('turbolinks:load', function() {
 
         eventClick: function(info) {
           if(confirm('削除しますか？'))  {
-              info.event.remove();
-          }
           $.ajax({
               type: 'DELETE',
               url: '/events/' + info.event.id,
-            //   data: { start: info.event.start,
-            //           end: info.event.end
-            //   },
-          });
+            });
+            info.event.remove();
+          }
         },
 
         dateClick: function(info) {
-             //クリックした日付の情報を取得
-            const year  = info.date.getFullYear();
-            const month = (info.date.getMonth() + 1);
-            const day   = info.date.getDate();
-
-            //ajaxでevents/newを着火させ、htmlを受け取る
             $.ajax({
                 type: 'GET',
                 url:  '/events/new',
             }).done(function (res) {
-                // 成功処理
-                // 受け取ったhtmlをさっき追加したmodalのbodyの中に挿入する
                 $('.modal-body').html(res);
-
-                //フォームの年、月、日を自動入力
-                $('#event_start_1i').val(year);
-                $('#event_start_2i').val(month);
-                $('#event_start_3i').val(day);
-
-                $('#event_end_1i').val(year);
-                $('#event_end_2i').val(month);
-                $('#event_end_3i').val(day);
-
-                $('.modal-body').fadeIn();
-
             }).fail(function (result) {
-                // 失敗処理
                 alert("failed");
             });
         }
-
     });
     //カレンダー表示
     calendar.render();
