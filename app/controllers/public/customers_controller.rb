f28e0499be697603a8f16c7574a4b5259bb51043
suzu784 <s.suzuki784@hotmail.com>
2022-08-29 03:15:50 +0000

    #175

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -32,13 +32,13 @@ class Public::CustomersController < ApplicationController
   end
 
   def followings
-    customer = Customer.find(params[:customer_id])
-		@customers = customer.followings
+    @customer = Customer.find(params[:customer_id])
+		@customers = @customer.followings
   end
 
   def followers
-    customer = Customer.find(params[:customer_id])
-		@customers = customer.followers
+    @customer = Customer.find(params[:customer_id])
+		@customers = @customer.followers
   end
 
   private
